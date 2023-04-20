953448bdbab36a75aa5442031018347c02e7dded
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-04-20 23:38:54 +0530

    firebase setup

--- src/components/registration/SignUp.jsx
+++ src/components/registration/SignUp.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const SignUp = () => {
+  const handleSubmit = () => {};
+
   return (
     <div>
-      <form className='signup-form'>
+      <form onClick={() => {}} className='signup-form'>
         <label htmlFor='name'>Enter your name</label>
         <input type='text' className='name' placeholder='Shivaji Raje' />
         <label htmlFor='email'>Enter your email</label>
         <input type='text' className='email' placeholder='abc@email.com' />
         <label htmlFor='password'>Enter your password</label>
         <input type='text' className='password' placeholder='125233qwer' />
+        <div>
+          <button type='submit'>Submit</button>
+        </div>
       </form>
     </div>
   );
