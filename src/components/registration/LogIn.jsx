ac9a3077b393707fc3db8ce181504fba57e82f54
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-04-20 15:12:44 +0530

    handle submit function created

--- src/components/registration/LogIn.jsx
+++ src/components/registration/LogIn.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 const LogIn = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    navigate('/sign');
+  };
+
   return (
     <div>
-      <form className='login-form'>
+      <form
+        onClick={(e) => {
+          handleSubmit();
+        }}
+        className='login-form'
+      >
         <label htmlFor='email'>Enter your email</label>
         <input type='text' className='email' placeholder='abc@email.com' />
         <label htmlFor='password'>Enter your password</label>
         <input type='text' className='password' placeholder='125233qwer' />
+        <div>
+          <button type='submit'>Submit</button>
+        </div>
       </form>
+      <div>
+        <p>not</p>
+        <NavLink to='/sign'>Signed up</NavLink>
+      </div>
     </div>
   );
 };
