5d13212c823355335847df25cc96ac1e5348391c
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-04-20 14:41:37 +0530

    login and sign up page created

--- src/components/registration/LogIn.jsx
+++ src/components/registration/LogIn.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const LogIn = () => {
-  return <div>LogIn</div>;
+  return (
+    <div>
+      <form className='login-form'>
+        <label for='email'>Enter your email</label>
+        <input type='text' className='email' placeholder='abc@email.com' />
+        <label for='password'>Enter your password</label>
+        <input type='text' className='password' placeholder='125233qwer' />
+      </form>
+    </div>
+  );
 };
 
 export default LogIn;
--- src/components/registration/SignUp.jsx
+++ src/components/registration/SignUp.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <form className='signup-form'>
+        <label for='name'>Enter your name</label>
+        <input type='text' className='name' placeholder='Shivaji Raje' />
+        <label for='email'>Enter your email</label>
+        <input type='text' className='email' placeholder='abc@email.com' />
+        <label for='password'>Enter your password</label>
+        <input type='text' className='password' placeholder='125233qwer' />
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
