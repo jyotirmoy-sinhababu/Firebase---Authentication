c734c47f09c812bf18b10b1a8fd46c177d2c208a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-04-22 10:25:29 +0530

    logout function created

--- src/pages/dashBoard/DashBoard.jsx
+++ src/pages/dashBoard/DashBoard.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { auth } from '../../firebaseConfig/FirebaseConfig';
+import { signOut } from 'firebase/auth';
 
 const DashBoard = () => {
+  const navigate = useNavigate();
+
+  //handle logout
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('logout successful');
+        navigate('/log');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <div>
-        <button>Logout</button>
+        <button onClick={handleSignOut}>Logout</button>
       </div>
       <div>
         <h1>Hello World</h1>
